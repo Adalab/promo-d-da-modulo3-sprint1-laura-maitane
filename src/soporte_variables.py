e9de4162a70362c2a1cb69c6838b4ededfee4838
MaitaneP <portillamaitane@gmail.com>
2023-05-07 20:37:01 +0200

    correcciones reg-lin 1-3

---
+++ src/soporte_variables.py
@@ -0,0 +1,7 @@
+# primera selección de columnas
+lista_columnas = ['country', 'Region', 'Population in thousands (2017)', 'GDP per capita (current US$)',
+                   'Employment: Agriculture (% of employed)', 'Employment: Industry (% of employed)', 'Employment: Services (% of employed)', 'Population age distribution (0-14 / 60+ years, %)',
+                    'International migrant stock (000/% of total pop.)', 'Education: Government expenditure (% of GDP)', 'Quality Of Life Index', 
+                    'Purchasing Power Index', 'Safety Index', 'Cost of Living', 'Rent Index', 'Grocery Index', 'Restaurant Price Index', 'Adjusted net national income per capita (constant 2010 US$)',
+                     'Consumer price index (2010 = 100)', 'Human capital index (HCI) (scale 0-1)', 'Inflation, consumer prices (annual %)', 'Urban population (% of total population)_y']
+
